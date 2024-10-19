f119072cd35f4809e779d570cf78a7fb27cf3db2
HARSHIT KUMAR MAURYA <104720283+Harshit029@users.noreply.github.com>
2024-10-19 19:52:20 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,16 @@
 const { Telegraf } = require('telegraf');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+  
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
 
 const TOKEN = '8051392949:AAFhvBmejUawfdCBjAahgKue3TPxAVH06CU';
 
